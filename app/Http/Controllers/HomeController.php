5f6a1b99a0f7fcae51519a33e908d78fa379e8f0
IT-JohnArian <malondrasarian@gmail.com>
2023-01-05 13:50:56 +0800

    Minor Update - Bug Fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,22 +24,22 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $uploadCount = 0;
-        $EncodeCount = 0;
-        $CheckedCount = 0;
+        $totalUploadCount = DB::select('SELECT COUNT(*) FROM documents');
+        $totalEncodeCount = DB::select('SELECT COUNT(*) FROM documents WHERE is_Encoded = ?', [1]);
+        $totalCheckedCount = DB::select('SELECT COUNT(*) FROM documents WHERE is_Checked = ?', [1]);
 
-        if(auth()->user()->id == 1){
-            $uploadCount = count((DB::select("SELECT * FROM documents WHERE created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
-            $EncodeCount = count((DB::select("SELECT * FROM documents WHERE is_Encoded = 1 AND created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
-            $CheckedCount = count((DB::select("SELECT * FROM documents WHERE is_Checked = 1 AND created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
-        }else{
-            $deptID = auth()->user()->department;
+        // if(auth()->user()->id == 1){
+        //     $uploadCount = count((DB::select("SELECT * FROM documents WHERE created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
+        //     $EncodeCount = count((DB::select("SELECT * FROM documents WHERE is_Encoded = 1 AND created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
+        //     $CheckedCount = count((DB::select("SELECT * FROM documents WHERE is_Checked = 1 AND created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
+        // }else{
+        //     $deptID = auth()->user()->department;
 
-            $uploadCount = count((DB::select("SELECT * FROM documents WHERE dept_id = $deptID AND created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
-            $EncodeCount = count((DB::select("SELECT * FROM documents WHERE dept_id = $deptID AND is_Encoded = 1 AND created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
-            $CheckedCount = count((DB::select("SELECT * FROM documents WHERE dept_id = $deptID AND is_Checked = 1 AND created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
-        }
+        //     $uploadCount = count((DB::select("SELECT * FROM documents WHERE dept_id = $deptID AND created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
+        //     $EncodeCount = count((DB::select("SELECT * FROM documents WHERE dept_id = $deptID AND is_Encoded = 1 AND created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
+        //     $CheckedCount = count((DB::select("SELECT * FROM documents WHERE dept_id = $deptID AND is_Checked = 1 AND created_at >= (LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL 1 MONTH) AND created_at <  (LAST_DAY(NOW()) + INTERVAL 1 DAY)")));
+        // }
 
-        return view('home', compact('uploadCount', 'EncodeCount', 'CheckedCount'));
+        return view('home', compact('totalUploadCount', 'totalEncodeCount', 'totalCheckedCount', 'uploadCount', 'EncodeCount', 'CheckedCount'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,15 +9,15 @@
         <div class="grid grid-cols-3 gap-5 pt-2">
             <div class="border border-neutral-200 shadow rounded-md h-18 p-2">
                 <h1 class="font-semibold">Total Upload</h1>
-                <h1 class="float-right text-2xl">{{ $uploadCount }}</h1>
+                <h1 class="float-right text-2xl">{{ $totalUploadCount }}</h1>
             </div>
             <div class="border border-neutral-200 shadow rounded-md h-18 p-2">
                 <h1 class="font-semibold">Total Encode</h1>
-                <h1 class="float-right text-2xl">{{ $EncodeCount }}</h1>
+                <h1 class="float-right text-2xl">{{ $totalEncodeCount }}</h1>
             </div>
             <div class="border border-neutral-200 shadow rounded-md h-18 p-2">
                 <h1 class="font-semibold">Total Quality Check</h1>
-                <h1 class="float-right text-2xl">{{ $CheckedCount }}</h1>
+                <h1 class="float-right text-2xl">{{ $totalCheckedCount }}</h1>
             </div>
         </div>
     </div>
